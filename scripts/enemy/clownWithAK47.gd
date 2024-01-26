ce3700b9758c4d6761a3238623a9b7baa80e9f9d
Cassie Parsons <2201287@uad.ac.uk>
2024-01-26 22:42:50 +0000

    Clown with AK47 can now shoot on a timer

--- scripts/enemy/clownWithAK47.gd
+++ scripts/enemy/clownWithAK47.gd
@@ -1,9 +1,24 @@
 extends "res://scripts/enemy/baseEnemy.gd"
 
+var Bullet:Node2D
+@export
+var attackSpeed:float=0.2
+@export
+var _moveSpeed:float = 0.5
+
+var attackTimer:float=0 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	super._process(delta)
-	print("Inherited print")
+	
 
-func start(_Player, _maxHealth, _moveSpeed):
-	super.start(_Player,_maxHealth,_moveSpeed)
+func start(_Player, _maxHealth):
+	super.start(_Player,_maxHealth)
+	moveSpeed=_moveSpeed
+	
+func attack(delta):	#Function called every frame
+	attackTimer+=delta
+	if(attackTimer>=attackSpeed):
+		attackTimer=0
+		print("Test")		#TODO: Add shooting bullet
