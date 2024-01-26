77079aab9496247c3b5a021075dc428e5aebf1ac
Cassie Parsons <2201287@uad.ac.uk>
2024-01-26 21:20:16 +0000

    moved base enemy and added first enemy type

--- scripts/enemy/baseEnemy.gd
+++ scripts/enemy/baseEnemy.gd
@@ -1,21 +1,31 @@
 extends Node2D
 
-var Player=null		#Node2D
+var Player:Node2D=null		#Node2D
 
-var health		#Float
-var maxHealth	#Float
+var health:float
+var maxHealth:float	
 
-func start(player):	#When enemy is created, add all 
-	player=Player
+var actionRate: float
+
+var moveSpeed: float
+
+func start(_Player, _maxHealth, _actionRate):	#When enemy is created, add all 
+	Player=_Player
+	maxHealth=_maxHealth
+	actionRate=_actionRate
 	
 func move():	#Overwrite to add specific enemy movement
 	pass
 	
 func spawnObject():	 #Acts as shooting
 	pass
+	
+func _process(delta):
+	pass
 
-func GetHurt(damage):
+func GetHurt(damage:int):
 	pass
 
 func onDeath():
 	pass
+
