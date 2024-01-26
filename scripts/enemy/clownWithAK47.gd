8b48a33eaf1261f68860fae340ee514d3b8d8d22
Cassie Parsons <2201287@uad.ac.uk>
2024-01-26 23:25:35 +0000

    Bullet offset should now work

--- scripts/enemy/clownWithAK47.gd
+++ scripts/enemy/clownWithAK47.gd
@@ -5,6 +5,13 @@ var Bullet:Node2D
 var attackSpeed:float=0.2
 @export
 var _moveSpeed:float = 0.5
+@export
+var fireCone:float = 1/4.0 * PI
+@export
+var fireChangeRate:float = 1/32.0 * PI
+
+var offset:float=0
+var fireChange=fireChangeRate
 
 var attackTimer:float=0 
 
@@ -20,8 +27,21 @@ func start(_Player, _maxHealth):
 func attack(delta):	#Function called every frame
 	attackTimer+=delta
 	if(attackTimer>=attackSpeed):
+		
+		var cosOffset:float=cos(offset)	#Get fire direction with offset
+		var sinOffset:float=sin(offset)
+		var attackDirection:Vector2=Vector2(cosOffset*playerDirection.x-sinOffset*playerDirection.y,sinOffset*playerDirection.x+cosOffset*playerDirection.y)
+		
+		offset+=fireChange#Adjust offset
+		
+		if(abs(offset)>fireCone):	#Flip direction of bullet movement
+			fireChange*=-1
+			offset+=fireChange
+		
+		velocity+=attackDirection * 120.0
+		move_and_slide()
 		attackTimer=0
-		print("Test")		#TODO: Add shooting bullet
+		print(offset)		#TODO: Add shooting bullet
 
 func move(delta):
 	velocity=playerDirection * moveSpeed
