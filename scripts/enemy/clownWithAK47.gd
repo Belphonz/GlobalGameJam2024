05498516499df7130480264bff0de42e1de9102f
Cassie Parsons <2201287@uad.ac.uk>
2024-01-27 00:04:11 +0000

    Enemy only fires when within range

--- scripts/enemy/clownWithAK47.gd
+++ scripts/enemy/clownWithAK47.gd
@@ -4,16 +4,29 @@ var Bullet:Node2D
 @export
 var attackSpeed:float=0.2
 @export
-var _moveSpeed:float = 0.5
+var firingTime:float=4.0
+@export 
+var cooldownTime:float = 1.0
+@export
+var _moveSpeed:float = 60
 @export
 var fireCone:float = 1/4.0 * PI
 @export
 var fireChangeRate:float = 1/32.0 * PI
 
+@export
+var firingRange:float=100.0
+@export
+var rangeRange:float=40.0
+
 var offset:float=0
 var fireChange=fireChangeRate
+var weaponActive:bool=true
+var movingToRange:bool=true
 
-var attackTimer:float=0 
+var attackTimer:float=0 		#Time between shots, time shooting continuously, time with weapon cooling down
+var attackingTimer:float=0
+var cooldownTimer:float=0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -25,9 +38,27 @@ func start(_Player, _maxHealth):
 	moveSpeed=_moveSpeed
 	
 func attack(delta):	#Function called every frame
-	attackTimer+=delta
-	if(attackTimer>=attackSpeed):
+	
+	var distFromPlayer:float=(Player.get_global_position().distance_to(get_global_position()))
+	
+	if(!weaponActive):	#If weapon is cooling down, handle cooldown timer, done before checking range as weapon will cooldown when 
 		
+		cooldownTimer+=delta
+		if(cooldownTimer>cooldownTime):
+			cooldownTimer=0
+			attackingTimer=0
+			weaponActive=true
+			
+	if(abs(distFromPlayer-firingRange)>rangeRange):	#If player is outside of firing range, don't attack
+		if(weaponActive):
+			attackingTimer=0
+		return
+	
+	
+	
+	attackTimer+=delta
+	attackingTimer+=delta;
+	if(attackTimer>=attackSpeed):
 		var cosOffset:float=cos(offset)	#Get fire direction with offset
 		var sinOffset:float=sin(offset)
 		var attackDirection:Vector2=Vector2(cosOffset*playerDirection.x-sinOffset*playerDirection.y,sinOffset*playerDirection.x+cosOffset*playerDirection.y)
@@ -42,6 +73,9 @@ func attack(delta):	#Function called every frame
 		move_and_slide()
 		attackTimer=0
 		print(offset)		#TODO: Add shooting bullet
+		
+	if(attackingTimer>=firingTime):	#Weapon deactivates after firing for too long
+		weaponActive=false
 
 func move(delta):
 	velocity=playerDirection * moveSpeed
