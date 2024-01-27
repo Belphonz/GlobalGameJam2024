8d794a579249c3c59fafab977eff680f40d463f2
GuestGoob <2203074@uad.ac.uk>
2024-01-27 00:07:32 +0000

    Added play area limit and collision with it

--- scripts/CharacterController.gd
+++ scripts/CharacterController.gd
@@ -6,11 +6,20 @@ var HP = 100.0
 @export var BULLET_SPEED = 400.0
 @export var FIRERATE = 0.2
 @export var PLAYER_ROTATION_SPEED = 80.0
+var move_direction
+
+
 
 func _physics_process(delta):
-	var move_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	move_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var point_direction = get_global_mouse_position()
 	look_at(point_direction)
 	point_direction = point_direction.normalized()
 	velocity = move_direction * MOVEMENT_SPEED
+	move_and_slide()
+
+
+func _on_area_2d_area_exited(area):
+	print("1")
+	velocity = -move_direction * MOVEMENT_SPEED
 	move_and_slide()
