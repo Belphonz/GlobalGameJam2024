9fc231a245eb92905c1a60c07931f3c0c486f892
Cassie Parsons <2201287@uad.ac.uk>
2024-01-27 18:30:51 +0000

    Jester throws bells

---
+++ scripts/bullets/jesterBell.gd
@@ -0,0 +1,41 @@
+extends Node2D
+
+@export 
+var airTime:float=0.4	#how long object is thrown for
+
+@export
+var maxCurveHeight=20
+
+var timeInAir:float=0
+
+var startThrow:Vector2
+var endThrow:Vector2
+
+var active:bool=true
+
+func throw(startLocation:Vector2,endLocation:Vector2):
+	active=true
+	startThrow=startLocation
+	endThrow=endLocation
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+		if(active):	#Move the bell in an arc
+			timeInAir+=delta
+			if(timeInAir>airTime):
+				active=false
+				set_global_position(endThrow)
+				return
+			var pointInCurve:float = timeInAir/airTime
+			var position:Vector2 = lerp(startThrow,endThrow,pointInCurve)
+			
+			var yOffset:float=pointInCurve*-4.0*maxCurveHeight*(1.0-pointInCurve)	#Calculate y offset to add for curve
+			position.y+=yOffset
+			
+			set_global_position(position)
