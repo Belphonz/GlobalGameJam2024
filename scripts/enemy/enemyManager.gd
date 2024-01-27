e9bc5e62682fb644b8882e67f1eab9264f5bf167
GuestGoob <2203074@uad.ac.uk>
2024-01-27 14:35:41 +0000

    clown

--- scripts/enemy/enemyManager.gd
+++ scripts/enemy/enemyManager.gd
@@ -13,14 +13,23 @@ var EnemyID = 0
 func _ready():
 	Player=get_node("../Player")
 	spawnClownAK47(Vector2(100,100))
+	#spawnRingmaster(Vector2(100,100))
 	
 	
 func spawnClownAK47(position:Vector2):
-	var enemyInstanceNode=cAK47.instantiate()
+	var enemyInstanceNode:CharacterBody2D = preload("res://elements/enemies/ClownWithAK47.tscn").instantiate()
 	enemyInstanceNode.set_global_position(position)
 	enemyInstanceNode.start(Player,1)
 	EnemyID += 1
 	enemyInstanceNode.name = "Enemy ClownAK47" + str(EnemyID)
+	add_child(enemyInstanceNode)
+	
+func spawnRingmaster(position:Vector2):
+	var enemyInstanceNode:CharacterBody2D = preload("res://elements/enemies/ringmaster.tscn").instantiate()
+	enemyInstanceNode.set_global_position(position)
+	enemyInstanceNode.start(Player,1)
+	EnemyID += 1
+	enemyInstanceNode.name = "Enemy Ringmaster" + str(EnemyID)
 	add_child(enemyInstanceNode)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
