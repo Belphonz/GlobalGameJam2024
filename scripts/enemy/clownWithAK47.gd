b25360353d5ecb93ad35781fbacdea3daa51694c
Cassie Parsons <2201287@uad.ac.uk>
2024-01-27 00:11:29 +0000

    Enemy now fires depending on the range
    Too close - Moves further back
    Too far - Moves closer
    Right range - Fires (has cooldown)

--- scripts/enemy/clownWithAK47.gd
+++ scripts/enemy/clownWithAK47.gd
@@ -10,6 +10,14 @@ var fireCone:float = 1/4.0 * PI
 @export
 var fireChangeRate:float = 1/32.0 * PI
 
+<<<<<<< Updated upstream
+=======
+@export
+var firingRange:float=400.0
+@export
+var rangeRange:float=100.0
+
+>>>>>>> Stashed changes
 var offset:float=0
 var fireChange=fireChangeRate
 
@@ -25,9 +33,33 @@ func start(_Player, _maxHealth):
 	moveSpeed=_moveSpeed
 	
 func attack(delta):	#Function called every frame
+<<<<<<< Updated upstream
 	attackTimer+=delta
 	if(attackTimer>=attackSpeed):
 		
+=======
+	
+	var distFromPlayer:float=(Player.get_global_position().distance_to(get_global_position()))
+	
+	if(!weaponActive):	#If weapon is cooling down, handle cooldown timer, done before checking range as weapon will cooldown when 
+		
+		cooldownTimer+=delta
+		if(cooldownTimer>cooldownTime):
+			cooldownTimer=0
+			attackingTimer=0
+			weaponActive=true
+			
+	if(abs(distFromPlayer-firingRange)>rangeRange):	#If player is outside of firing range, don't attack
+		if(weaponActive):
+			attackingTimer=0
+		return
+	
+	
+	attackTimer+=delta	#Update timers
+	attackingTimer+=delta;
+	
+	if(attackTimer>=attackSpeed && weaponActive):
+>>>>>>> Stashed changes
 		var cosOffset:float=cos(offset)	#Get fire direction with offset
 		var sinOffset:float=sin(offset)
 		var attackDirection:Vector2=Vector2(cosOffset*playerDirection.x-sinOffset*playerDirection.y,sinOffset*playerDirection.x+cosOffset*playerDirection.y)
@@ -38,11 +70,24 @@ func attack(delta):	#Function called every frame
 			fireChange*=-1
 			offset+=fireChange
 		
-		velocity+=attackDirection * 120.0
-		move_and_slide()
 		attackTimer=0
+<<<<<<< Updated upstream
 		print(offset)		#TODO: Add shooting bullet
+=======
+		print("offsetFIRE")		#TODO: Add shooting bullet
+		
+	if(attackingTimer>=firingTime):	#Weapon deactivates after firing for too long
+		weaponActive=false
+>>>>>>> Stashed changes
 
 func move(delta):
-	velocity=playerDirection * moveSpeed
+	
+	var distFromPlayer:float=(Player.get_global_position().distance_to(get_global_position()))
+	
+	if(abs(distFromPlayer-firingRange)<rangeRange):	##Enemy is at optimal range, don't move
+		return
+		
+	var direction:int=sign(distFromPlayer-firingRange)
+	
+	velocity=playerDirection * moveSpeed * direction
 	move_and_slide()
