4741cd10f4109af22e89194d79119afb3e57be42
Aethel B. Crown <aethel.crown@gmail.com>
2024-01-27 04:10:35 +0000

    Bullets which shoot correctly

--- scripts/player/CharacterController.gd
+++ scripts/player/CharacterController.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 var HP:float = 100.0
 @export var MAX_HP:float = 100.0
 @export var MOVEMENT_SPEED:float = 300.0
+@export var BULLET_BOUNCE_COUNT:int = 3
 @export var BULLET_SPEED:float = 400.0
 @export var FIRE_RATE:float = 1
 var CURRENT_FIRE_RATE = 0
@@ -11,21 +12,23 @@ var move_direction
 
 func Controller():
 	var move_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	var point_direction = get_global_mouse_position()
-	look_at(point_direction)
-	point_direction = point_direction.normalized()
 	velocity = move_direction * MOVEMENT_SPEED
 	move_and_slide()
 	
 func Shoot(delta):
+	var shootPoint = get_child(1) as Node2D
 	CURRENT_FIRE_RATE = CURRENT_FIRE_RATE + delta
+	
+	var facingdirection = (get_global_mouse_position() - global_position).normalized()
+	shootPoint.rotation = facingdirection.angle()
 	if Input.is_action_pressed("shoot") and CURRENT_FIRE_RATE > FIRE_RATE:
 		var bulletInstance:CharacterBody2D = preload("res://elements/bullets/bullet.tscn").instantiate()
 		bulletInstance.speed = BULLET_SPEED
-		bulletInstance.direction = (get_global_mouse_position() - global_position).normalized()
-		bulletInstance.maxBounceCount = 3
+		bulletInstance.direction = facingdirection
+			
+		bulletInstance.maxBounceCount = BULLET_BOUNCE_COUNT
 		
-		bulletInstance.position = (get_child(1) as Node2D).global_position
+		bulletInstance.global_position = shootPoint.global_position + (facingdirection * 20)
 		get_parent().add_child(bulletInstance)
 		CURRENT_FIRE_RATE = 0
 	
