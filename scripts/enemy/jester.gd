b52c5bd24fc464400d01dc7474b2a10e50586b49
Cassie Parsons <2201287@uad.ac.uk>
2024-01-28 14:13:32 +0000

    Jester spikes now spawn above bullets

--- scripts/enemy/jester.gd
+++ scripts/enemy/jester.gd
@@ -53,7 +53,7 @@ func attack(delta):
 			#Throw trap
 			var NailHazard=nailHazard.instantiate()
 			NailHazard.throw(get_global_position(),get_global_position()+throwDirection*nailThrowDistance)
-			get_node("../../BulletObject").add_child(NailHazard)
+			get_node("../../SpikeNode").add_child(NailHazard)
 		else:
 			
 			#Throw bell
