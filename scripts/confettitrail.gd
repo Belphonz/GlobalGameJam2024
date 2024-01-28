479cd4a6daa48b65b807eac244a70093cead40db
Aethel B. Crown <aethel.crown@gmail.com>
2024-01-28 11:10:08 +0000

    Confetti Generation

---
+++ scripts/confettitrail.gd
@@ -0,0 +1,11 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	rotation = randf_range(-180,180)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
